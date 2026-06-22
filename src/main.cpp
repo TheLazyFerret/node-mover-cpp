54791272e598c0b9bf89cd853a5bc6e3222f8716
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-06-22 12:55:46 +0100

    Make the methods of node inline. Use weak_ptr instead of shared_ptr in the Edges

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 
 #include "./node/node.hpp"
 
-int main(int argc, char** argv) {
+//void recursive_deep_search(const std::shared_ptr<Node> initial_node) {}
+
+int main(/*int argc, char** argv*/) {
   std::shared_ptr<Node> node1 = std::make_shared<Node>(1, 1);
   std::shared_ptr<Node> node2 = std::make_shared<Node>(2, 2);
   std::shared_ptr<Node> node3 = std::make_shared<Node>(3, 3);
@@ -13,13 +15,12 @@ int main(int argc, char** argv) {
   node1->add_edge({node2});
   node1->add_edge({node3});
 
-
-  const auto node1_edges = node1->get_edges();
-  for (const auto& edge: node1_edges) {
-    if (edge.node == node1) {
-      std::println("same node");
-    }
+  const auto node1_set = node1->get_edges();
+  for (const auto& edge: node1_set) {
+    const auto pos = edge.node.lock()->get_pos();
+    std::println("pos: {}, {}", pos.first, pos.second);
   }
+
 
   return 0;
 }
--- src/node/node.hpp
+++ src/node/node.hpp
@@ -11,31 +11,37 @@ class Node; // Fordward declaration.
 
 /// Edge helper struct.
 struct Edge {
-  std::shared_ptr<Node> node;
-  std::uint32_t cost = 0;
-  friend bool operator<(const Edge& a, const Edge& b) noexcept;
+  std::weak_ptr<Node> node;
+  std::int32_t cost = 0;
+  friend bool operator<(const Edge& a, const Edge& b);
 };
 
 /// Node class.
 class Node {
 public:
   /// Special methods.
-  inline Node() noexcept : pos_x(0), pos_y(0) {}
-  inline explicit Node(std::int32_t x, std::int32_t y) noexcept : pos_x(x), pos_y(y) {}
+  inline Node() noexcept : pos(0, 0) {}
+  inline explicit Node(std::int32_t x, std::int32_t y) noexcept : pos(x, y) {}
   // Implicit default copy, move and destructor.
 
   /// Public methods.
-  std::pair<std::int32_t, std::int32_t> get_pos() const noexcept;
-  void set_pos(std::int32_t, std::int32_t) noexcept;
+  inline std::pair<std::int32_t, std::int32_t> get_pos() const noexcept { return pos; }
+  inline void set_pos(std::int32_t x, std::int32_t y) noexcept { pos = {x, y}; };
 
-  void add_edge(const Edge edge) noexcept;
+  inline void add_edge(const Edge edge) noexcept {adjacent_nodes.insert(edge);}
   inline const std::set<Edge>& get_edges() const noexcept {return adjacent_nodes;};
   
 private:
   /// Private methods.
   
   /// Attributes.
-  std::int32_t pos_x;
-  std::int32_t pos_y;
+  std::pair<std::int32_t, std::int32_t> pos;
   std::set<Edge> adjacent_nodes;
 };
+
+///  "Less than" operator for Edge.
+inline bool operator<(const Edge& a, const Edge& b) {
+  const auto temp_shared_a = a.node.lock();
+  const auto temp_shared_b = b.node.lock();
+  return temp_shared_a < temp_shared_b;
+}
