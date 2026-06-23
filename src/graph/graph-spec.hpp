99b90e57d6c3f46cd506a2ac95355ae8d0d13b77
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-06-23 09:22:37 +0100

    add graph specification

---
+++ src/graph/graph-spec.hpp
@@ -0,0 +1,29 @@
+//! Axuliar stucture for an easy parsable graph configuration.
+
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <vector>
+
+/// Auxiliar specification of a node's edge.
+struct EdgeSpec {
+  std::int32_t cost;        // Cost of the edge.
+  std::size_t destiny_node; // Destiny node.
+  // The origin node is the NodeSpec itself.
+};
+
+/// Auxiliar specification of a node.
+struct NodeSpec {
+  std::optional<std::string> name;                // Optional name.
+  std::pair<std::int32_t, std::int32_t> position; // Node coordinates position.
+  std::vector<EdgeSpec> edges;
+};
+
+/// Graph specification auxiliar structure.
+struct GraphSpec {
+  std::optional<std::string> name; // Optional name.
+  std::size_t number_of_nodes;
+  std::vector<NodeSpec> nodes;
+};
